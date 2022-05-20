0199ada76864a61e2123b3ca7a201cdd75d21cdc
Valentina-Mangini-Cassutti <mangini.cassutti.valentina@gmail.com>
2022-05-20 05:09:55 -0300

    correcciones

--- Portfolio-ManginiCassuttiValentina/src/app/app.module.ts
+++ Portfolio-ManginiCassuttiValentina/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { ComponentsComponent } from './components/components.component';
 import { GitHubComponent } from './components/git-hub/git-hub.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoApComponent,
     ComponentsComponent,
     GitHubComponent,
-    BannerComponent
+    BannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
